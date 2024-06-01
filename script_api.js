f9c0c9761fd79e05594eb64864f68ecc66045ede
unknown <andresoktubre86@gmail.com>
2024-05-31 23:33:58 -0300

    Agrego otra llamada a la API para Aclamadas. Falta la solicitud post de busqueda

--- script_api.js
+++ script_api.js
@@ -1,20 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     const apiKey = 'efde8e832636a3245ee8595d26d6dbf4';
-    const apiUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=' + apiKey + '&language=es-ES';
+    const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=es-ES`;
+    const apiUrl_top = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`;
     const options = {method: 'GET', headers: {accept: 'application/json'}};
 
     fetch(apiUrl, options)  
         .then(response => response.json())
         .then(data => {
             console.log(data); 
-            displayMovies(data,"tendenciasContainer");
-            displayMovies(data,"peliculasAclamadas");
-            
-            
+            displayMovies(data, "tendenciasContainer");
         })
-        .catch(error => console.error('Error al obtener los datos:', error));
-    
-    function displayMovies(data,container_id) {
+        .catch(error => console.error('Error al obtener los datos de películas populares:', error));
+
+    fetch(apiUrl_top, options)  
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); 
+            displayMovies(data, "aclamadasContainer");
+        })
+        .catch(error => console.error('Error al obtener los datos de películas aclamadas:', error));
+
+    function displayMovies(data, container_id) {
         const movies = data.results;
         const container = document.getElementById(container_id);
         container.innerHTML = ''; 
